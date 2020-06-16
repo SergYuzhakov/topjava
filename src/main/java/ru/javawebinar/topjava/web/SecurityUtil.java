9de90c8c258c8bb239f5fc6715746e4f8a6286ae
SergYuzhakov <sergyuzhakov@gmail.com>
2020-06-16 21:21:11 +0300

    Add choice authorized User

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -1,11 +1,22 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.slf4j.LoggerFactory.getLogger;
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
+    private static final Logger log = getLogger(SecurityUtil.class);
+    private static Integer userId = 1;
 
     public static int authUserId() {
-        return 1;
+        return  userId;
+    }
+
+    public static void setUserId(int id){
+        log.info("Set authUserId = {}", id);
+        SecurityUtil.userId = id;
     }
 
     public static int authUserCaloriesPerDay() {
--- src/main/webapp/users.jsp
+++ src/main/webapp/users.jsp
@@ -1,4 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html lang="ru">
 <head>
     <title>Users</title>
@@ -7,5 +9,10 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Users</h2>
+    <form action="users" method="post">
+<p>User: <%= request.getParameter("user") %></p>
+        <input type="hidden" name="userId" value=<%= request.getParameter("user") %>>
+    <button type="submit">Apply</button>
+</form>
 </body>
 </html>
